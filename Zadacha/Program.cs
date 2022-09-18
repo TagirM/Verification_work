0c145a8c48a11de5db208ce7cf5323d633e4ee2f
Admin <tmurzagaleev@yandex.ru>
2022-09-18 22:45:38 +0700

    дополнил решение задачи в С#

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -5,18 +5,22 @@ string A2 = "hi";
 string A3 = "goodmoning";
 string A4 = "buy";
 
-string[] array= new string[] {A1,A2,A3,A4};
-string A5 = string.Join("; ",array);
+string[] array = new string[] { A1, A2, A3, A4 };
+string A5 = string.Join("; ", array);
 System.Console.WriteLine($"Задан массив из строк: {A5} ");
 System.Console.Write("Новый массив строк с длиной символов не более 3: ");
 int i = 0; // i - index(int i = 0)
+int n = 0;
 int arraySize = array.Length; // Длина массива - количество элементов
 
 while (i < arraySize)
 {
-    if (array[i].Length <= 3) System.Console.Write($"{array[i]}; ");
+    if (array[i].Length <= 3)
+    {
+        System.Console.Write($"{array[i]}; ");
+        n++;
+    }
     i++;
 }
-
-
+if (n == 0) System.Console.Write("таких строк нет");
 
